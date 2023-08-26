7f07774def2f78982abeea305a145b793d107655
TMK1210 <133574124+TMK1210@users.noreply.github.com>
2023-08-26 15:32:08 +0700

    2-3.c

--- 2-3.c
+++ 2-3.c
@@ -8,5 +8,8 @@ int main()
     printf("Enter a hexadecimal number: ");
     scanf("%s", hexString);
     Int = strtol(hexString, NULL, 16);
+    //strtol:hàm dùng để chuyển đổi từ hệ 16 về hệ 10
     printf("Integer number: %ld\n", Int);
+    
+    return 0;
 }
